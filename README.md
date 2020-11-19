4d03eaddf22ad58216073cb3b04c1f543ad52b1e
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-11-19 11:22:21 +0100

    update versions

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cdn
 PHP Class for generating head links and scripts to resources hosted on various CDN's
+### libraries
+- Angular JS
+- Bootstrap
+- D3
+- Del CSS
+- Dojo
+- FontAwesome
+- jQuery
+- jQueryUI
+### usage
+Simply echo where required:
+```php
+echo Del\Cdn::angularJavascript('1.5.6');
+// output 
+// <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+```
+
+### changelog
+Default versions have changed in v2.x, just pass in the old version number where you call the code.
+
+For instance, if you used Bootstrap v3.3.2, just pass in `3.3.2`:
+```php
+echo Cdn::bootstrapJavascript(); // now gives v4.5.3
+echo Cdn::bootstrapJavascript('3.3.2'); // you are now upgraded to Del\Cdn v2.x
+``` 
+
--- src/Cdn.php
+++ src/Cdn.php
@@ -14,7 +14,7 @@ class Cdn
      * @param string $version
      * @return string
      */
-    public static function angularJavascript($version = '1.5.0')
+    public static function angularJavascript($version = '1.5.6')
     {
         switch($version)
         {
@@ -29,10 +29,13 @@ class Cdn
      * @param string $version
      * @return string
      */
-    public static function bootstrapCssLink($version = '3.3.2')
+    public static function bootstrapCssLink($version = '4.5.3')
     {
         switch($version)
         {
+            case '4.5.3':
+                return '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">';
+                break;
             case '2.3.2':
                 return '<link type="text/css" rel="stylesheet" media="screen" href="//maxcdn.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css">';
                 break;
@@ -47,10 +50,13 @@ class Cdn
      * @param string $version
      * @return string
      */
-    public static function bootstrapJavascript($version = '3.3.2')
+    public static function bootstrapJavascript($version = '4.5.3')
     {
         switch($version)
         {
+            case '4.5.3':
+                return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>';
+                break;
             case '2.3.2':
                 return '<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>';
                 break;
@@ -81,7 +87,7 @@ class Cdn
      * @param string $version
      * @return string
      */
-    public static function jQueryJavascript($version = '2.1.3')
+    public static function jQueryJavascript($version = '3.5.1')
     {
         return '<script src="//ajax.googleapis.com/ajax/libs/jquery/'.$version.'/jquery.min.js"></script>';
     }
@@ -90,7 +96,7 @@ class Cdn
      * @param string $version
      * @return string
      */
-    public static function jQueryUiCssLink($version = '1.11.3')
+    public static function jQueryUiCssLink($version = '1.12.1')
     {
         return '<link type="text/css" rel="stylesheet" media="screen" href="//ajax.googleapis.com/ajax/libs/jqueryui/'.$version.'/themes/smoothness/jquery-ui.css">';
     }
@@ -99,8 +105,26 @@ class Cdn
      * @param string $version
      * @return string
      */
-    public static function jQueryUIJavascript($version = '1.11.3')
+    public static function jQueryUIJavascript($version = '1.12.1')
     {
         return '<script src="//ajax.googleapis.com/ajax/libs/jqueryui/'.$version.'/jquery-ui.min.js"></script>';
+    }
+
+    /**
+     * @param string $version
+     * @return string
+     */
+    public static function d3Javascript($version = '6.2.0')
+    {
+        return '<script src="https://ajax.googleapis.com/ajax/libs/d3js/'.$version.'/d3.min.js"></script>"></script>';
+    }
+
+    /**
+     * @param string $version
+     * @return string
+     */
+    public static function dojoJavascript($version = '1.13.0')
+    {
+        return '<script src="https://ajax.googleapis.com/ajax/libs/dojo/'.$version.'/dojo/dojo.js"></script>';
     }
 }
